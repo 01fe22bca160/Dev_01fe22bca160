20caea16ea960b0a7efdb63198275b3de9f49d59
01fe22bca160 <01fe22bca160@kletech.ac.in>
2024-03-28 20:16:29 +0530

    Employee

---
+++ Employee.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+// Employee class
+class Employee {
+    private String name;
+    private int employeeId;
+    protected double salary;
+
+    // Constructor
+    public Employee(String name, int employeeId, double salary) {
+        this.name = name;
+        this.employeeId = employeeId;
+        this.salary = salary;
+    }
+
+    // Display employee details
+    public void displayDetails() {
+        System.out.println("Employee ID: " + employeeId);
+        System.out.println("Name: " + name);
+        System.out.println("Salary: $" + salary);
+    }
+}
+
